75b18584cb5e6003859b288428557bbd4cec0ec3
Sarthak <sarthak.cmk.al@gmail.com>
2024-11-03 15:23:36 +0530

    enter fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "393d57ab56a5de78095df92421234b96",
+const RESOURCES = {"flutter_bootstrap.js": "36ee678d26cde625dd12fcaaa580fa82",
 "version.json": "197fd70b4d0da3246f5345f747a23176",
 "index.html": "91eb28293775f1ab21a28dadedda80d6",
 "/": "91eb28293775f1ab21a28dadedda80d6",
-"main.dart.js": "70e975702590db6bfe56459ef578eaef",
+"main.dart.js": "465749119a2ca92c7ef32547e929da6d",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
