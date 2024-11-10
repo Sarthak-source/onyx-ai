3ac46cddd8cafaaff3ab8254cd904f65d65af2ef
Sarthak <sarthak.cmk.al@gmail.com>
2024-11-10 17:19:41 +0530

    color- text fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "82113e449cf27796fe1440c82029813f",
+const RESOURCES = {"flutter_bootstrap.js": "844fb83394d92deabf163fb689217296",
 "version.json": "197fd70b4d0da3246f5345f747a23176",
 "index.html": "91eb28293775f1ab21a28dadedda80d6",
 "/": "91eb28293775f1ab21a28dadedda80d6",
-"main.dart.js": "ee54f12c201cb81b9ded6aca82a459f8",
+"main.dart.js": "68bd6502062a4facc2310720923286c6",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
