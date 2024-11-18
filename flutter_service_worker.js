4872536d471d8191660cec441e3b487ab8d30093
Sarthak <sarthak.cmk.al@gmail.com>
2024-11-18 18:38:25 +0530

    hi

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "360e45d261e8ffc81fca5bc3f2db1c98",
+const RESOURCES = {"flutter_bootstrap.js": "30dedd72e7f7474a9ab2df660c58c201",
 "version.json": "197fd70b4d0da3246f5345f747a23176",
 "index.html": "91eb28293775f1ab21a28dadedda80d6",
 "/": "91eb28293775f1ab21a28dadedda80d6",
-"main.dart.js": "ea84fd9d5eccb91696957df603022abf",
+"main.dart.js": "136ce415e12a67ba4856108a42650816",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
